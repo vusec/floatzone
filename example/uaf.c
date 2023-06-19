437db2426f4e7ba37722966a9dc55fd65d6022e5
florenzio pizza <floriski@hotmail.nl>
2023-06-19 18:27:23 +0000

    Added UAF example

---
+++ example/uaf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void fill_buffer(char *buf){
+        memset(buf, 0x41, 16);
+}
+
+char buggy(unsigned int idx) {
+        char* buf = malloc(16);
+        fill_buffer(buf);
+        free(buf);
+        return buf[idx];
+}
+
+int main(int argc, char **argv) {
+        printf("%c\n", buggy(atoi(argv[1])));
+}
